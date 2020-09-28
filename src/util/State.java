ac6b217bfeed579b85583ebf1e149a458a42cd36
KYG-APPS <kyg.apps@gmail.com>
2020-09-27 22:26:33 -0400

    (NF) Implementation of TreeNode

--- src/util/State.java
+++ src/util/State.java
@@ -4,7 +4,7 @@ package util;
  * State.java
  * @author Kevin Gao on 2020-9-27
  * 
- * Standardizes the return values of TreeNodes
+ * Enum that standardizes the return values of TreeNodes
  */
 public enum State {
 	
---
+++ src/util/TreeNode.java
@@ -0,0 +1,70 @@
+package util;
+
+import java.util.ArrayList;
+
+/**
+ * TreeNode.java
+ * @author Kevin Gao on 2020-9-27
+ *
+ * Abstract class that serves as the superclass of all Nodes
+ */
+public abstract class TreeNode {
+	
+	/** ArrayList of of this TreeNode's children, stored in order */
+	private ArrayList<TreeNode> children;
+
+	/** Custom descriptor of this specific TreeNode instance */
+	private String descriptor;
+	
+	/** The TreeNodeType of this Node */
+	private TreeNodeType type;
+	
+	/**
+	 * Constructor for TreeNode
+	 * @param descriptor - custom description of the purpose of this TreeNode
+	 * @param type - TreeNodeType of this TreeNode
+	 */
+	public TreeNode(String descriptor, TreeNodeType type) {
+		children = new ArrayList<TreeNode>();
+		this.descriptor = descriptor;
+		this.type = type;
+	}
+	
+	/**
+	 * @return the number of children this TreeNode has
+	 */
+	public int numChildren() {
+		return this.children.size();
+	}
+	
+	/**
+	 * @param index the index of the child to retrieve
+	 * @return the child of this TreeNode at the specified index
+	 */
+	public TreeNode getChild(int index) {
+		return this.children.get(index);
+	}
+	
+	/**
+	 * @return description of the purpose of this TreeNode
+	 */
+	public String getDescriptor() {
+		return this.descriptor;
+	}
+	
+	/**
+	 * @return TreeNodeType of this TreeNode
+	 */
+	public TreeNodeType getTreeNodeType() {
+		return this.type;
+	}
+	
+	/**
+	 * To be implemented for each subclass of TreeNode
+	 * Executes the instructions as specified by the behavior of the subclass
+	 * 
+	 * @return the resulting State (success/failure/running) of the TreeNode
+	 */
+	public abstract State run();
+	
+}
