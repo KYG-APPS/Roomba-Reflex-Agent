7bc646c191b057c1f2180d1264d5e65e97487a1b
KYG-APPS <kyg.apps@gmail.com>
2020-09-27 21:52:49 -0400

    Sanity Check

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * main.java
+ * @author Kevin Gao (kgao03) on 2020-9-27
+ * 
+ * Driver class for the Roomba Reflex Agent
+ * Creates a Roomba instance and simulates cycles
+ */
+public class Main {
+
+	/**
+	 * Main function of the program
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
