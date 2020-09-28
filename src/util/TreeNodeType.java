7485878593563ee6c574c91645d17de7c27fcf4d
KYG-APPS <kyg.apps@gmail.com>
2020-09-27 22:05:40 -0400

    (NF) Implementation of TreeNodeType with Placeholder Class

---
+++ src/util/TreeNodeType.java
@@ -0,0 +1,44 @@
+package util;
+
+/**
+ * TreeNodeType.java
+ * @author Kevin Gao on 2020-9-27
+ * 
+ * Enum for linking the different types of TreeNodes with their subclasses
+ * e.g. Selection, Sequence, Until Fail, etc.
+ */
+public enum TreeNodeType {
+	
+	/** Initialization of all TreeNodes with their corresponding descriptors */
+	PLACEHOLDER("Test", Tempt.class);
+	
+	/** Identifier String for the TreeNode */
+	private final String identifier;
+	
+	/** Corresponding Class of the TreeNode */
+	private final Class<?> classType;
+	
+	/**
+	 * Constructor for TreeNodeType
+	 * @param identifier - Identifier String for the TreeNode
+	 * @param classType - Corresponding Class of the TreeNode
+	 */
+	TreeNodeType(String identifier, Class<?>  classType){
+		this.identifier = identifier;
+		this.classType = classType;
+	}
+	
+	/**
+	 * @return the identifier string associated with this TreeNodeType
+	 */
+	public String getIdentifier() {
+		return this.identifier;
+	}
+	
+	/**
+	 * @return the Class associated with this TreeNodeType
+	 */
+	public Class<?> getClassType(){
+		return this.classType;
+	}
+}
